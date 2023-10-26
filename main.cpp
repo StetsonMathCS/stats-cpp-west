b585b579fa20657cafda2d0e600bd0af6b3c2f45
Joshua Eckroth <joshuaeckroth@gmail.com>
2023-10-26 09:35:17 -0400

    Revert back to simple data reading, assume clean file

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <iterator>
-#include <sstream>
 #include "mode_calculator.h"
 
 int main() {
     ModeCalculator calculator;
-    std::string line;
-    std::stringstream ss;
     float val;
-    while(std::cin >> line) {
-        ss.clear();
-        ss << line;
-        if(ss >> val) {
-            calculator.handleValue(val);
-        }
+    while(std::cin >> val) {
+        calculator.handleValue(val);
     }
 
     float mode = calculator.getResult();
